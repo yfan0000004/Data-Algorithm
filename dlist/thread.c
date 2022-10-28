ac5d374aebfed83953a181efdb4b21684bc19f78
yfan997521804@qq.com <yfan997521804@qq.com>
2022-10-28 16:51:57 +0800

    实践线程互斥锁，条件变量

--- dlist/thread.c
+++ dlist/thread.c
@@ -1,21 +1,28 @@
+/*
+线程互斥锁，条件变量的实践
+*/
 #include <unistd.h>
 #include <pthread.h>
 #include <time.h>
+#include <semaphore.h>
 
 #include"dlist.h"
 
 pthread_mutex_t listlock;
+pthread_cond_t cond;
 
 void *ReadList(void *args)
 {
+    int j = 40;
     struct list* list = (struct list*)args;
     while(1){
         pthread_mutex_lock(&listlock);
+        while(list->next == NULL) pthread_cond_wait(&cond, &listlock);
         list_deleteAtTail(list);
         printf("删除元素后的链表：\n");
         printList(list);
         pthread_mutex_unlock(&listlock);
-        sleep(2);
+       
     }
     
 }
@@ -24,6 +31,7 @@ void *WriteList(void *args)
 {
     //生成一个随机数
     srand(time(0)); 
+    int j = 40;
     struct list* list = (struct list*)args;
     while(1){
         int a = 1 + (rand() % 1000);
@@ -32,13 +40,15 @@ void *WriteList(void *args)
         printf("加入元素：%d 后的链表：\n", a);
         printList(list);
         pthread_mutex_unlock(&listlock);
-        sleep(2);
+        pthread_cond_signal(&cond);
+        
     }
 }
 
 int32_t main()
 {
-    pthread_mutex_init(&listlock, NULL);
+    pthread_mutex_init(&listlock, NULL); 
+    pthread_cond_init(&cond, NULL);
     pthread_t g_threadId1;
     pthread_t g_threadId2;
 
@@ -59,7 +69,16 @@ int32_t main()
     pthread_create(&g_threadId1, NULL, WriteList, (void*)list);
     pthread_create(&g_threadId2, NULL, ReadList, (void*)list);
 
-    sleep(20);
+    sleep(1);
+    
+    //这是一种简单的线程同步的方式
+    pthread_join(g_threadId2, NULL);//等待线程1结束
+    pthread_join(g_threadId1, NULL);//等待线程2结束
+
+    
+
+
+    sleep(2);
 
     return 1;
 }
